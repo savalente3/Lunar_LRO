995186cb786cf8d09fed5ccc5b1fb303e33a5214
Sofia Valente <sa.valente3@gmail.com>
2026-08-30 13:45:49 +0100

    final results v1 model with wac+dem and training fo wac

--- 4_training/train.py
+++ 4_training/train.py
@@ -52,8 +52,8 @@ keras.utils.set_random_seed(SEED)
 params = {
     'dataset': DATASET,
     'dim': 256,
-    'channels': 'both',                 # 'both' | 'wac' | 'dem'
-    'input_channels': 2,                # 2 for both, 1 for ablations
+    'channels': 'wac',                 # 'both' | 'wac' | 'dem'
+    'input_channels': 1,                # 2 for both, 1 for ablations
     'n_filters': 32,                    # v1's n filters -> baseline overrides this
     'FL': 3,                            # kernel size
     'init': 'he_normal',
